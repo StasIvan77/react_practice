7a0ff6e479ff9e68efb4d64065d096bab238c1d9
Ivan Stasiv <stasivan77@gmail.com>
2020-10-21 02:36:05 +0300

    Creating some interactive checkboxes

---
+++ src/Todo/AddTodo.js
@@ -0,0 +1,42 @@
+import React, { useState } from "react";
+import PropTypes from 'prop-types'
+
+function useInputValue(defultValue='') {
+    const [value, setValue] = useState(defultValue)
+
+    return {
+        bind: {
+
+            value,
+            onChange: event => setValue(event.target.value)
+        },
+        clear: () => setValue(''),
+        value: () => value
+    }
+}
+
+function AddTodo ({ onCreate }) {
+    const input = useInputValue('')
+    const [value, setValue] = useState('')
+    function submitHandler(event) {
+        event.preventDefault()
+
+        if (input.value().trim()) {
+            onCreate (input.value())
+            input.clear()
+        }
+    }
+
+    return (
+        <form style={{marginBottom: '1rem'}} onSubmit={submitHandler}>
+            <input {...input.bind}/>
+            <button type='submit'>Add todo</button>
+        </form>
+    )
+}
+
+AddTodo.propType = {
+    onCreate: PropTypes.func.isRequired
+    }
+
+export default AddTodo
